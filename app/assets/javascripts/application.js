1fce13de6c79a9574f6cfabc2ea304c2028bdf26
ilarinie <ilari.nieminen@outlook.com>
2016-07-13 09:50:33 +0300

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require ui-bootstrap-1.3.3
+//= require angular.min
+//= require mainlist
 //= require_tree .
+
