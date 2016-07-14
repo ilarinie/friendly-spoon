c054317fc366839dade56c8abc914da05ad96032
ilarinie <ilari.nieminen@outlook.com>
2016-07-14 11:31:00 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 units = Unit.create([{name: 'Cup'}, {name: 'dl'}, {name: 'l'}, {name: 'tsp'}, {name: 'tbsp'}, {name: 'mm'}, {name: 'g'}, {name: 'pound'}, {name: 'oz'} ,{name: 'pcs'}])
 durations = Duration.create([{range: '0min - 30min'}, {range: '30min - 1h'}, {range: '1h - 1h 30min'}, {range: '1h 30min - 2h'}, {range: '2h+'}])
 levels = Level.create([{id:1, level: 'Easy'}, {id:2, level: 'Medium'}, {id:3, level: 'Difficult'}])
+users = User.create([{name: "Ilari", username: "ile", password: "1234", password_confirmation: "1234"}, {name: "Bebe", username: "bebe", password: "murmeli", password_confirmation: "murmeli"}])
