72751babd73f69b179c25d50db6092647bfafe0b
ilarinie <ilari.nieminen@outlook.com>
2016-07-15 15:39:40 +0300

    Add files via upload

--- friendly spoon html demo/js/app.js
+++ friendly spoon html demo/js/app.js
@@ -1,8 +1,14 @@
 var apper = angular.module("apper", []);
 
-apper.controller("IncController", function($scope) {
-	$scope.incs = ["milk","flour","bananas"];
+apper.controller("IncController", function($scope, $location) {
+	$scope.incs = [{name: "milk", id:1},{name: "flour",id:2},{name: "bananas", id:3}];
+	$scope.units = ["cup","dl","l"];
 	$scope.checked = [];
+	
+	console.log("kea: "+$scope.recipeinc)
+	var path = window.location.pathname;
+	
+	console.log(path );
 	
 	$scope.toggleInc = function (index) {
 		$scope.checked.push($scope.incs[index]);
@@ -13,4 +19,12 @@ apper.controller("IncController", function($scope) {
 		$scope.incs.push.apply($scope.incs, $scope.checked);
 		$scope.checked = [];
 	};
+	
+	$scope.submitMyForm = function(recipeinc, index){
+		recipeinc.inc_id = index
+		console.log(recipeinc)
+		
+		var data = $scope.recipeinc;
+	
+	}
 });
