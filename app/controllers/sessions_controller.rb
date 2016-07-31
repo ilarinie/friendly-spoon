49465da1611e328c5c94e362e4d9f3c019495629
ilarinie <ilari.nieminen@outlook.com>
2016-07-31 18:11:58 +0300

    new and improved login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,21 @@
 class SessionsController < ApplicationController
+
   def new
+    render :layout => false
   end
 
   def create
     user = User.find_by username: params[:username]
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to :root, notice: "Signed in succesfully!"
+      redirect_to :root, notice: "Logged in succesfully! Welcome back "+current_user.to_s+"!"
     else
-      redirect_to :back, notice: "Username and/or password mismatch"
+      redirect_to :back, alert: "Username and/or password mismatch"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to :root, notice: 'Signed out succesfully.'
+    redirect_to :login, notice: 'Logged out succesfully. Have a nice day!'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
     has_many :recipes
     has_many :notes
 
-
+    def to_s
+        name
+    end
 
 end
